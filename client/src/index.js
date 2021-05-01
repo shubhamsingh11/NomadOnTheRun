ee69c16c14c72fe872be8359fce5a17d65b84529
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-05-02 04:24:48 +0530

    Added client side

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+//Connecting div by the id of root in index.html
+ReactDOM.render(<App />, document.getElementById('root'));
